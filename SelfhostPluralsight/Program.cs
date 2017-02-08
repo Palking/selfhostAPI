4b86466c1ede2d28dfd8e5eec6ca579405e296db
Palking <blah.olaf@web.de>
2017-02-08 17:30:19 +0100

    initial login

---
+++ SelfhostPluralsight/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.SelfHost;
+using System.Web.Http;
+
+namespace SelfhostPluralsight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpSelfHostConfiguration config = new HttpSelfHostConfiguration("http://localhost:8899");
+            config.Routes.MapHttpRoute("default",
+                "api/{controller}/{id}",
+                new { id = RouteParameter.Optional });
+            var server = new HttpSelfHostServer(config);
+            var task = server.OpenAsync();
+            task.Wait();
+            Console.WriteLine("Server up and running..");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ SelfhostPluralsight/RecipesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SelfhostPluralsight
+{
+    public class RecipesController : ApiController
+    {
+        public string Get()
+        {
+            return "Hello from my Controller";
+        }
+    }
+}
